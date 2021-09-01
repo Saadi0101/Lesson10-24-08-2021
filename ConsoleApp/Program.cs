d29f4e204f5449d6298b7e83c9156d9f6afa582d
Sa'di <88400372+Saadi0101@users.noreply.github.com>
2021-09-01 23:01:51 +0500

    Add files via upload

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите валюту usd, euro, rub, som: ");
+            string v1 = Console.ReadLine();
+            string v2 = "";
+            if (v1 == "som")
+            {
+                Console.WriteLine("Введиту вторую валюту usd, euro, rub: ");
+                v2 = Console.ReadLine();
+
+                Console.WriteLine("Введите сумму конвертации: ");
+                double s = Convert.ToDouble(Console.ReadLine());
+                Converter converter = new Converter() { m1 = v1, sum = s, m2 = v2 };
+                converter.GetConvert();
+            }
+            else if (v1 == "usd" || v1 == "euro" || v1 == "rub" )
+            {
+                Console.WriteLine("Вторая волюта som! ");
+                Console.WriteLine("Введите сумму конвертации: ");
+                double s = Convert.ToDouble(Console.ReadLine());
+                Converter converter = new Converter() { m1 = v1, sum = s, m2 = v2 };
+                converter.GetConvert();
+            }
+            else Console.WriteLine("Введена не правильная валюта!");
+        }
+    }
+
+
+    class Converter
+    {
+        public string m1;
+        public string m2;
+        public double result;
+        public double sum;
+        public Converter() { }
+
+        public void GetConvert()
+        {
+            if(m1 == "usd")
+            {
+                result = sum * 11.44;
+                Console.WriteLine($"{sum} дол. = {Math.Round(result, 2)} сом.");
+            }
+            else if (m1 == "euro")
+            {
+                result = sum * 13.45;
+                Console.WriteLine($"{sum} эвро = {Math.Round(result, 2)} сом.");
+            }
+            else if (m1 == "rub")
+            {
+                result = sum * 0.1535;
+                Console.WriteLine($"{sum} рубл. = {Math.Round(result, 2)} сом.");
+            }
+            else if (m1 == "som" && m2 == "rub")
+            {
+                result = sum / 0.1535;
+                Console.WriteLine($"{sum} сом. = {Math.Round(result, 2)} рубл.");
+            }
+            else if (m1 == "som" && m2 == "usd")
+            {
+                result = sum / 11.44;
+                Console.WriteLine($"{sum} сом. = {Math.Round(result, 2)} дол.");
+            }
+            else if (m1 == "som" && m2 == "euro")
+            {
+                result = sum / 13.45;
+                Console.WriteLine($"{sum} сом. = {Math.Round(result, 2)} эвро");
+            }
+
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee("Андрей", "Малахов", "Журналист", 1500);
+            employee1.GetInfo();
+
+            Employee employee2 = new Employee("Фёдр", "Бандарчук", "Режессёр", 2500);
+            employee2.GetInfo();
+
+            Employee employee3 = new Employee("Ольга", "Бузова", "Певица", 5000);
+            employee3.GetInfo();
+
+        }
+    }
+    
+    class Employee
+    {
+        public string name;
+        public string lasName;
+        public int zp;
+        public double nal;
+        public double penFon;
+        public double chzp;
+        public string dol;
+
+        public Employee () { }
+        public Employee(string Name, string LastName, string Dol, int ZP)
+        {
+            name = Name;
+            lasName = LastName;
+            zp = ZP;
+            dol = Dol;
+            
+        }
+
+        public void GetInfo()
+        {
+            nal = zp * 13 / 100;
+            penFon = zp * 1 / 100;
+            chzp = zp - nal - penFon;
+            Console.WriteLine($"Имя: {name}\nФамилия: {lasName}\nДолжность: {dol}\nЗ/П: {zp}\nНалог: {nal}\nПенсионный фонд: {penFon}\nЧистая З/П: {chzp}");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+
+    }
+}
